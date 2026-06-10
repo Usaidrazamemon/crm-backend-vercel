a5259b7bf1b98632a867b1feaf92dbe8fb448423
SakinaMemon <memonusaidraza71@gmail.com>
2026-06-10 05:40:08 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,9 +7,16 @@ const connectDB = require("../config/db");
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
-  credentials: false,
+  origin: [
+    "https://crm-frontend-seven-steel.vercel.app",
+    "http://localhost:3000"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
+app.options("*", cors());
 
 app.use(express.json());
 
